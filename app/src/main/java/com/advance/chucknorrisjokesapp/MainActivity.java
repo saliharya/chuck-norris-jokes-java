e40beecf0ec26c7f33006776bbb7bc22860be0ec
Salih Arya <saliharyazinc@gmail.com>
2025-10-22 23:49:06 +0700

    feat: Initial project setup
    - Initialize a new Android application named "ChuckNorrisJokesApp".
    - Implement view binding and edge-to-edge display in `MainActivity`.

---
+++ app/src/main/java/com/advance/chucknorrisjokesapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.advance.chucknorrisjokesapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.advance.chucknorrisjokesapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupViewBinding();
+        setupEdgeToEdge();
+    }
+
+    private void setupViewBinding() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    private void setupEdgeToEdge() {
+        EdgeToEdge.enable(this);
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
